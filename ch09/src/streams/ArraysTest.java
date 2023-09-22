6c6edc0a7124a4ba314c86ca261d54dd1ad7bc69
kiyongee2 <kiyongee2@gmail.com>
2023-09-22 18:15:10 +0900

    람다식, 스트림

---
+++ ch09/src/streams/ArraysTest.java
@@ -0,0 +1,23 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraysTest {
+
+	public static void main(String[] args) {
+		Integer[] num1 = {3, 1, 2, 4, 5};
+	
+		//오름차순 정렬 - Arrays.sort()
+		Arrays.sort(num1);
+		System.out.println(Arrays.toString(num1));
+		
+		//오름차순 정렬된 배열을 뒤집기
+		Collections.reverse(Arrays.asList(num1));
+		System.out.println(Arrays.toString(num1));
+		
+		//num1의 요소 중 3개 복사 - Arrays.copyOf()
+		Integer[] num2 = Arrays.copyOf(num1, 3); //num1의 요소를 3개 복사
+		System.out.println(Arrays.toString(num2));
+ 	}
+}
